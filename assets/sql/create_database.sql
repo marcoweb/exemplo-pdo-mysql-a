60a51bc4ec8b23ac0911fc63caaf0d74c2c6fb23
marcoweb <marcocarvalho.web@gmail.com>
2021-09-13 14:28:05 +0000

    Insert

--- assets/sql/create_database.sql
+++ assets/sql/create_database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE biblioteca;
 
 USE biblioteca;
 
-CREATE TABLE generos(my
+CREATE TABLE generos(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL
 );
---
+++ insert.php
@@ -0,0 +1,27 @@
+<?php
+require_once './vendor/autoload.php';
+
+use PDO;
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $bd = new PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');
+    $comando = $bd->prepare('INSERT INTO generos(nome) VALUES (:nome)');
+    $comando->execute([':nome' => $_POST['nome']]);
+    header('Location:index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Novo Gênero</h1>
+        <form action="insert.php" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" />
+            <button type="button">Salvar</button>
+        </form>
+    </body>
+</html>
