b9182269a4bfeae55a13eb3a77200f3d2d4d387e
marcoweb <marcocarvalho.web@gmail.com>
2021-09-30 17:47:58 +0000

    Bootstrap

--- insert.php
+++ insert.php
@@ -15,13 +15,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     <head>
         <meta charset="utf-8">
         <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     </head>
     <body>
-        <h1>Novo Gênero</h1>
-        <form action="insert.php" method="post">
-            <label for="nome">Nome</label>
-            <input type="text" name="nome" />
-            <button type="submit">Salvar</button>
-        </form>
+        <main class="container">
+            <h1>Novo Gênero</h1>
+            <form action="insert.php" method="post">
+                <div class="form-group">
+                    <label for="nome">Nome</label>
+                    <input class="form-control" type="text" name="nome" />
+                </div>
+                <br />
+                <button class="btn btn-success" type="submit">Salvar</button>
+            </form>
+        </main>
     </body>
 </html>
